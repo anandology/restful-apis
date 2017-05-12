f81458e7e69f9d9b962cd1ba1e9fef6a47448944
Anand Chitipothu <anandology@gmail.com>
2017-05-13 01:54:36 +0530

    Added helloworld webapp

---
+++ helloworld/hello.py
@@ -0,0 +1,22 @@
+import web
+
+urls = (
+    "/", "index",
+    "/hello", "hello"
+)
+
+app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        web.header("Content-Type", "text/plain")
+        return "Welcome!\n"
+
+class hello:
+    def GET(self):
+        i = web.input(name="World")
+        web.header("Content-Type", "text/plain")
+        return "Hello {}!\n".format(i.name)
+
+if __name__ == "__main__":
+    app.run()
